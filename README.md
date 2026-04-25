f755e00619782f87f3c60f438fd51c823be6c9e4
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2026-04-25 15:22:26 +0100

    feat: Initialize Project Skeleton

---
+++ README.md
@@ -0,0 +1,3 @@
+# Meeds MCP Server
+
+This is a project to implement MCP Server Specification with OAuth Integration. It contains an API which will be used by Meeds based projects to define MCP Tools.
---
+++ mcp-server-webapp/src/main/java/io/meeds/ai/mcp/server/McpServerApplication.java
@@ -0,0 +1,66 @@
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2025 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package io.meeds.ai.mcp.server;
+
+import org.springframework.ai.mcp.server.autoconfigure.McpServerStreamableHttpWebMvcAutoConfiguration;
+import org.springframework.ai.mcp.server.common.autoconfigure.annotations.StatelessServerSpecificationFactoryAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.elasticsearch.ReactiveElasticsearchClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import io.meeds.spring.AvailableIntegration;
+import io.meeds.spring.kernel.PortalApplicationContextInitializer;
+
+@SpringBootApplication(scanBasePackages = {
+  AvailableIntegration.KERNEL_MODULE,
+  McpServerApplication.TOOLS_MODULE_NAME,
+  McpServerApplication.COMMON_MODULE_NAME,
+  McpServerApplication.MODULE_NAME,
+}, exclude = {
+  LiquibaseAutoConfiguration.class,
+  DataSourceAutoConfiguration.class,
+  DataSourceTransactionManagerAutoConfiguration.class,
+  HibernateJpaAutoConfiguration.class,
+  SecurityAutoConfiguration.class,
+  ElasticsearchClientAutoConfiguration.class,
+  ElasticsearchRestClientAutoConfiguration.class,
+  ReactiveElasticsearchClientAutoConfiguration.class,
+  StatelessServerSpecificationFactoryAutoConfiguration.class,
+  McpServerStreamableHttpWebMvcAutoConfiguration.class,
+})
+@EnableWebSecurity
+@PropertySource("classpath:application.properties")
+@PropertySource("classpath:mcp-server.properties")
+public class McpServerApplication extends PortalApplicationContextInitializer {
+
+  public static final String MODULE_NAME        = "io.meeds.ai.mcp.server";
+
+  public static final String COMMON_MODULE_NAME = "io.meeds.ai.common";
+
+  public static final String TOOLS_MODULE_NAME  = "io.meeds.ai.tool";
+
+}
